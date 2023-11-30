4809a8d08be7959f8ca57b7ec37ac5587985e65f
MemphisCornelius <accounts.marvi@mailbox.org>
2023-11-30 22:32:07 +0100

    initial commit

---
+++ src/main/java/de/marvi/adventofcode/Main.java
@@ -0,0 +1,11 @@
+package de.marvi.adventofcode;
+
+import de.marvi.adventofcode.days.Day01;
+import dev.jayy.aochelper.Solver;
+
+public class Main {
+    public static void main(String[] args) {
+        var solver = new Solver();
+        solver.solve(Day01.class);
+    }
+}
---
+++ src/main/java/de/marvi/adventofcode/days/Day01.java
@@ -0,0 +1,14 @@
+package de.marvi.adventofcode.days;
+
+import dev.jayy.aochelper.utils.Day;
+
+public class Day01 extends Day {
+    protected Day01() {
+        super(2023, 1);
+    }
+
+    @Override
+    public Object partOne() {
+        return "The Answer";
+    }
+}
